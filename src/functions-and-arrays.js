05670f870f10637af37edb9d2eddec24d359a1fc
Joshua Emmanuel <joshuaemmanuel925@gmail.com>
2023-09-11 15:21:43 +0100

    Finish Iteration 3

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -25,7 +25,14 @@ function findLongestWord(words) {
 // Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    const eachNumber = numbers[i];
+    sum += eachNumber;
+  }
+  return sum;
+}
 
 
 

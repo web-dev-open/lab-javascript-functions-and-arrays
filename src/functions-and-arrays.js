5c9a6ae00fc22335000f4011670db9eb4e3881f1
Gabriella Neri <99314986+GabN-hub@users.noreply.github.com>
2023-11-16 13:54:39 +0800

    done iteration 8 - bonus

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -195,7 +195,30 @@ const matrix = [
   [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
 ];
 
-function greatestProduct() {}
+function greatestProduct() {
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+  let maxProduct = 0;
+
+  // Check horizontally
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j <= cols - 4; j++) {
+      const product = matrix[i][j] * matrix[i][j + 1] * matrix[i][j + 2] * matrix[i][j + 3];
+      maxProduct = Math.max(maxProduct, product);
+    }
+  }
+
+  // Check vertically
+  for (let i = 0; i <= rows - 4; i++) {
+    for (let j = 0; j < cols; j++) {
+      const product = matrix[i][j] * matrix[i + 1][j] * matrix[i + 2][j] * matrix[i + 3][j];
+      maxProduct = Math.max(maxProduct, product);
+    }
+  }
+  return maxProduct;
+}
+let maxProductAnswer = greatestProduct();
+console.log(maxProductAnswer);
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
